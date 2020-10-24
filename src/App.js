09bb8858329c8d77017a1e0b399d31b0712d0d11
Khusboo <bothrakhushboo2405@gmail.com>
2020-10-24 23:01:25 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+import Content from "./rootComponents/Content";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <Content />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/rootComponents/Content.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Content = () => {
+    return (
+        <div>
+            <h1>What is Lorem Ipsum?</h1>
+            <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+            </p>
+            <button class="btn btn-primary">Read More</button>
+        </div>
+    )
+}
+
+export default Content;
